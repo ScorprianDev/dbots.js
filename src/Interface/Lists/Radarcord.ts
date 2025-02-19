e0bcf81e7a97b0675b4e313743daa71245f0cf50
Scorprian <43557305+ScorprianDev@users.noreply.github.com>
2025-02-20 08:26:48 +1300

    Update and rename RadarBotList.ts to Radarcord.ts

--- src/Interface/Lists/Radarcord.ts
+++ src/Interface/Lists/Radarcord.ts
@@ -2,33 +2,33 @@ import { Service, ServicePostOptions } from '../Service'
 import { Util, IDResolvable } from '../../Utils/Util'
 
 /**
- * Represents the Radar Bot Directory service.
- * @see https://docs.radarbotdirectory.xyz/
+ * Represents the Radarcord service.
+ * @see https://docs.radarcord.net/
  */
-export default class RadarBotDirectory extends Service {
+export default class Radarcord extends Service {
   /** The values that can be used to select the service. */
   static get aliases() {
-    return ['radar', 'radarbots', 'radarbotdirectory.xyz']
+    return ['radar', 'radarcord', 'radarcord.net']
   }
 
   /** The logo URL. */
   static get logoURL() {
-    return 'https://theoverlook.xyz/Images/RBD/botlistlogo.png'
+    return 'https://radarcord.net/static/logo.png'
   }
 
   /** Service's name. */
   static get serviceName() {
-    return 'Radar Bot Directory'
+    return 'Radarcord'
   }
 
   /** The website URL. */
   static get websiteURL() {
-    return 'https://radarbotdirectory.xyz'
+    return 'https://radarcord.net'
   }
 
   /** The base URL of the service's API. */
   static get baseURL() {
-    return 'https://radarbotdirectory.xyz/api'
+    return 'https://radarcord.net/api'
   }
 
   /**
@@ -38,8 +38,8 @@ export default class RadarBotDirectory extends Service {
   static post(options: ServicePostOptions) {
     const { token, clientID, serverCount, shard } = options
     return super._post({
-      method: 'patch',
-      url: `/bot/${Util.resolveID(clientID)}/stats/`,
+      method: 'post',
+      url: `/bot/${Util.resolveID(clientID)}/stats`,
       headers: { Authorization: token },
       data: shard
         ? {
@@ -58,20 +58,4 @@ export default class RadarBotDirectory extends Service {
     return this._request({ url: `/bot/${Util.resolveID(id)}` })
   }
 
-  /**
-   * Gets the Unix Epoch Timestamp of the last time this user voted for this bot on this service.
-   * @param botID The bot's ID
-   * @param userID The user's ID
-   */
-  getBotVotes(botID: IDResolvable, userID: IDResolvable) {
-    return this._request(
-      {
-        url: `/lastvoted/${Util.resolveID(userID)}/${Util.resolveID(botID)}`,
-        headers: { Authorization: this.token }
-      },
-      {
-        requiresToken: true
-      }
-    )
-  }
 }
